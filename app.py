270922da5e6e7c17992ad213c9a222e382e956c8
gwillig <gustavwillig@gmail.com>
2019-11-23 19:30:10 +0000

    FIX: app.route('/venue')

--- app.py
+++ app.py
@@ -306,37 +306,26 @@ def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
   current_time = datetime.now().strftime('%Y-%m-%d %H:%S:%M')
-  # city_state = Venue.query.with_entities(Venue.city,Venue.state).group_by(Venue.state, Venue.city).all()
-  # all_venues= Venue.query.filter(Show.start_time > current_time).all()
-  # # Get all veneu`s
-  # for el in  city_state:
-  #   all_venues.shows.filter(Show.start_time > current_time).all()
+  db.session.rollback()
+  db.metadata.clear()
+  city_state = db.session.query(Venue.state, Venue.city).group_by(Venue.state, Venue.city).all()
+
+  result=[]
+  '1.Step: Filter for each location to get the venues'
+  for v in city_state:
+    v_result={"city":v.city,"state":v.state,"venues":[]}
+    venues_of_v = db.session.query(Venue.name,Venue.id).filter_by(city=v.city, state=v.state).all()
+    print(venues_of_v)
+    for v_v in venues_of_v:
+      venue_dict={}
+      venue_dict["id"]=v_v[1]
+      venue_dict["name"] = v_v[0]
+      venue_dict["num_upcoming_shows"] = db.session.query(Show).filter(Show.start_time >= current_time).filter_by(venue_id = v_v[1]).count()
+      v_result["venues"].append(venue_dict)
+    result.append(v_result)
 
 
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  },
-    {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-
-  return render_template('pages/venues.html', areas=data);
+  return render_template('pages/venues.html', areas=result);
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
@@ -357,7 +346,6 @@ def search_venues():
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
-
   data1={
     "id": 1,
     "name": "The Musical Hop",
@@ -365,7 +353,12 @@ def show_venue(venue_id):
     "address": "1015 Folsom Street",
     "city": "San Francisco",
     "state": "CA",
-
+    "phone": "123-123-1234",
+    "website": "https://www.themusicalhop.com",
+    "facebook_link": "https://www.facebook.com/TheMusicalHop",
+    "seeking_talent": True,
+    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
     "past_shows": [{
       "artist_id": 4,
       "artist_name": "Guns N Petals",
@@ -432,7 +425,6 @@ def show_venue(venue_id):
   }
   data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
   return render_template('pages/show_venue.html', venue=data)
-
 #  Create Venue
 #  ----------------------------------------------------------------
 
@@ -690,7 +682,8 @@ def shows():
     "artist_name": "The Wild Sax Band",
     "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
     "start_time": "2035-04-15T20:00:00.000Z"
-  }]
+  }
+  ]
   return render_template('pages/shows.html', shows=data)
 
 @app.route('/shows/create')

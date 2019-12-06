64099e64e79f98d354d54e38990cd858e0c20084
gwillig <gustavwillig@gmail.com>
2019-12-06 20:30:10 +0100

    FEAT: create_show_submission

--- app.py
+++ app.py
@@ -635,7 +635,7 @@ def edit_venue(venue_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
+    # Done: take values from the form submitted, and update existing
     # venue record with ID <venue_id> using the new attributes
     try:
         form = VenueForm(request.form)
@@ -739,15 +739,26 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-    # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
+    try:
+        new_show = {
+            "artist_id": request.form['artist_id'],
+            "venue_id": request.form['venue_id'],
+            "start_time": request.form['start_time'],
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        }
+        s1 = Show(**new_show)
+        db.session.add(s1)
+        db.session.commit()
+        flash('Show at ' + request.form['start_time'] + ' was successfully listed!')
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash('An error occurred. Show at ' + request.form['start_time'] + ' could not be listed.')
+    finally:
+        db.session.close()
+
     return render_template('pages/home.html')
+
 
 
 @app.errorhandler(404)
